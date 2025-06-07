b8f51582da26eb48f6159054906aa400726472ce
Living Neutral <jffrmcdonald@gmail.com>
2025-06-07 15:01:49 -0700

    adding personal project video

--- app/data.ts
+++ app/data.ts
@@ -29,21 +29,12 @@ type SocialLink = {
 
 export const PROJECTS: Project[] = [
   {
-    name: 'Motion Primitives Pro',
+    name: 'Personal Website revamp',
     description:
-      'Advanced components and templates to craft beautiful websites.',
-    link: 'https://pro.motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+      'Revamping the personal website using the NIM template',
+    link: 'https://github.com/Jeff-The-McD/mcds-portfolio',
+    video:'https://pub-b59b329d480a4e97bdbc453fc0d8ba34.r2.dev/2025-06-07%2014-18-59.mp4',
     id: 'project1',
-  },
-  {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
-    id: 'project2',
   },
 ]
 
