e2f5ebab0d5273639ac7406632be63da3b2bc513
Living Neutral <jffrmcdonald@gmail.com>
2025-06-07 15:09:58 -0700

    adding links from cloudflare media setup

--- app/data.ts
+++ app/data.ts
@@ -36,6 +36,14 @@ export const PROJECTS: Project[] = [
     video:'https://pub-b59b329d480a4e97bdbc453fc0d8ba34.r2.dev/2025-06-07%2014-18-59.mp4',
     id: 'project1',
   },
+    {
+    name: 'Optical Flow Template',
+    description:
+      'An Optical Flow project meant to be used with UAV\'s to help with object avoidance.',
+    link: 'https://github.com/Jeff-The-McD/mcds-portfolio',
+    video:'https://pub-b59b329d480a4e97bdbc453fc0d8ba34.r2.dev/CAL%20POLY%20Pomona%20Optflow%20video%20showcase.mp4',
+    id: 'project1',
+  },
 ]
 
 export const WORK_EXPERIENCE: WorkExperience[] = [
