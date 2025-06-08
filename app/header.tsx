36fa3a75d4a1ab917dc970afba3db53cef738256
Living Neutral <jffrmcdonald@gmail.com>
2025-06-07 21:29:16 -0700

    adding new subtitle

--- app/header.tsx
+++ app/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <header className="mb-8 flex items-center justify-between">
       <div>
         <Link href="/" className="font-medium text-black dark:text-white">
-          Jeffrey McDonald
+         <h1>Jeffrey McDonald</h1>
         </Link>
         <TextEffect
           as="p"
