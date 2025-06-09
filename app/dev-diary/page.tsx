0f14b37ac0f9e3ce3840b113bc3377ae96d0b477
Living Neutral <jffrmcdonald@gmail.com>
2025-06-08 18:35:47 -0700

    adding comments for scaffolding

--- app/dev-diary/page.tsx
+++ app/dev-diary/page.tsx
@@ -1,5 +1,12 @@
+
+
 export default function DevDiary(){
     return (
         <h1>Under Constructions</h1>
     )
+
+    // add main .
+    // add section
+    // add div
+    // add blog post item
 }
